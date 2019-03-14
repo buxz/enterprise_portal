13654ee914dc2c8b07dcd4d40f4d6199b218775b
SQ_BXZ <1016862070@qq.com>
2019-03-14 15:15:40 +0800

    后端集成完毕

--- src/main/java/com/buxz/enterprise_portal/controller/FrontController.java
+++ src/main/java/com/buxz/enterprise_portal/controller/FrontController.java
@@ -10,15 +10,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "/front")
 public class FrontController {
 
-    @RequestMapping(value = "/{path}", method = RequestMethod.POST)
+    @RequestMapping(value = "/{path}")
     public String path(@PathVariable String path) {
-        return "admin/" + path;
+        return "front/" + path;
     }
 
     @RequestMapping(value = "/newsFrontDetail", method = RequestMethod.POST)
     public String newsFrontDetail(Integer newsId, Model model) {
         model.addAttribute("newsId", newsId);
-        return "admin/newsFrontDetail";
+        return "front/newsFrontDetail";
     }
 
 }
